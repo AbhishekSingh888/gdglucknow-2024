36a2851e07be3e794733c8e9a9dd692aa3331f4a
Abhishek Singh <absingh@arcadsoftware.com>
2024-11-01 08:44:50 +0530

    Refactor pricing card component and pricing page layout

--- components/pricing/pricingcard.vue
+++ components/pricing/pricingcard.vue
@@ -1,67 +1,68 @@
 <template>
-    <v-card :class="['pricing-card', { 'highlight-card': highlight }]" elevation="3">
-      <v-card-title class="title">{{ title }}</v-card-title>
-      <v-card-subtitle class="price">{{ price }}</v-card-subtitle>
-      <v-card-text>
-        <ul class="features">
-          <li v-for="(feature, index) in features" :key="index">{{ feature }}</li>
-        </ul>
-      </v-card-text>
-      <v-card-actions>
-        <v-btn class="start-btn" color="primary" outlined>{{ buttonText }}</v-btn>
-      </v-card-actions>
-    </v-card>
-  </template>
-  
-  <script>
-  export default {
+    <v-container :class="['pricing-card', { 'highlight-card': highlight }]">
+        <v-card-title class="title">{{ title }}</v-card-title>
+        <v-card-subtitle class="price">{{ price }}</v-card-subtitle>
+        <v-card-text>
+            <ul class="features">
+                <li v-for="(feature, index) in features" :key="index">{{ feature }}</li>
+            </ul>
+        </v-card-text>
+        <v-card-actions>
+            <v-btn class="start-btn" color="primary" outlined>{{ buttonText }}</v-btn>
+        </v-card-actions>
+    </v-container>
+</template>
+
+<script>
+export default {
     props: {
-      title: String,
-      price: String,
-      features: Array,
-      buttonText: String,
-      highlight: Boolean,
+        title: String,
+        price: String,
+        features: Array,
+        buttonText: String,
+        highlight: Boolean,
     },
-  };
-  </script>
-  
-  <style scoped>
-  .pricing-card {
+};
+</script>
+
+<style scoped>
+.pricing-card {
     padding: 20px;
-    background-color: #f3f4f6;
+    background-color: #fff;
     color: #333;
     text-align: center;
-  }
-  
-  .highlight-card {
+    box-shadow: -10px 10px 0px 0px rgb(0 0 0);
+    border: 1px solid #e0e0e0;
+}
+
+.highlight-card {
     background-color: #6a0dad;
     color: #fff;
-  }
-  
-  .title {
+}
+
+.title {
     font-weight: bold;
     font-size: 1.2em;
     margin-bottom: 10px;
-  }
-  
-  .price {
+}
+
+.price {
     font-size: 2em;
     font-weight: bold;
     margin-bottom: 10px;
-  }
-  
-  .features {
+}
+
+.features {
     list-style: none;
     padding: 0;
     margin: 0;
-  }
-  
-  .features li {
+}
+
+.features li {
     margin-bottom: 8px;
-  }
-  
-  .start-btn {
+}
+
+.start-btn {
     margin-top: 20px;
-  }
-  </style>
-  
+}
+</style>
